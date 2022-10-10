8027a704bd5ab24bf203845403a04ef490e98b96
bamburik <bamburov.001@mail.ru>
2022-10-11 03:27:01 +0400

    fix getting auth token

--- src/main/java/org/bamburov/payments/PayPal.java
+++ src/main/java/org/bamburov/payments/PayPal.java
@@ -1,5 +1,6 @@
 package org.bamburov.payments;
 
+import io.restassured.response.Response;
 import org.bamburov.config.Props;
 import io.restassured.http.ContentType;
 import io.restassured.http.Header;
@@ -15,29 +16,51 @@ public class PayPal {
     //private static String paypalBusinessSecret = "EFW2pazNffXmOiYAuX63QpVThHO3qr_03KEn0NeRxseD2hbHeYM42rtzvDzcm5Q5sauN9de2KOHKXE-G";
 
     public static String getToken() {
-        return "Bearer " + given()
+         Response response = given()
                 .auth()
+                 .preemptive()
                 .basic(Props.getPaypalBusinessClientId(), Props.getPaypalClientSecret())
-                .header(new Header("Authorization", "Basic QVptdDBmU1F6YVVzM3ZzcjZsU2Z6UDRNa2djeWJlN20xeFd2eU9NbnJ4X1p4bWdJLW44Ny0xTTRpOHFoWFdJdzVqanJiQWFKWWpvR2FlTGo6RUZjakNtblcwRFd2YW9lemxiMGU0LUVDbVM5c2c1UWM2cVhlMjV5dl9GR2UxdmNJZlRyb3BuWjlCZGVPNEZfS0RORWZaVWF6bFNhODB2clk="))
                 .contentType(ContentType.URLENC)
                 .formParam("grant_type", "client_credentials")
-                .post(Props.getPaypalUrl() + "v1/oauth2/token").body().jsonPath().getString("access_token");
+                 .when()
+                .post(Props.getPaypalUrl() + "v1/oauth2/token");
+         if (response.statusCode() >= 300 || response.statusCode() < 200) {
+             System.out.println("client id - " + Props.getPaypalBusinessClientId());
+             System.out.println("client secret - " + Props.getPaypalClientSecret());
+             System.out.println("error on getting paypal token");
+             System.out.println("response body - " + response.body().prettyPrint());
+         }
+        return "Bearer " + response.body().jsonPath().getString("access_token");
     }
 
     public static String createInvoiceAndGetItsLink(String customerEmail, String serviceName, String totalAmount) {
-        String apiLink = given()
+        String requestBody = String.format(PayPal.createDraftInvoiceBodyFormat, PayPal.getNextInvoiceNumber(), customerEmail, serviceName, totalAmount);
+        System.out.println("getting api link to bill body");
+        System.out.println(requestBody);
+        Response response = given()
                 .header(new Header("Authorization", PayPal.getToken()))
                 .contentType(ContentType.JSON)
-                .body(String.format(PayPal.createDraftInvoiceBodyFormat, PayPal.getNextInvoiceNumber(), customerEmail, serviceName, totalAmount))
-                .post(Props.getPaypalUrl() + "v2/invoicing/invoices").body().jsonPath().getString("href");
+                .body(requestBody)
+                .post(Props.getPaypalUrl() + "v2/invoicing/invoices");
+        if (response.statusCode() >= 300 || response.statusCode() < 200) {
+            System.out.println("error on getting api link to bill");
+            System.out.println("response body - " + response.body().prettyPrint());
+        }
+        String apiLink = response.body().jsonPath().getString("href");
         String[] splittedlink = apiLink.split("/");
         String invoiceId = splittedlink[splittedlink.length - 1];
 
-        return given()
+        response = given()
                 .header(new Header("Authorization", PayPal.getToken()))
                 .contentType(ContentType.JSON)
                 .body("{\"send_to_invoicer\": true}")
-                .post(Props.getPaypalUrl() + "v2/invoicing/invoices/" + invoiceId + "/send").body().jsonPath().getString("href");
+                .post(Props.getPaypalUrl() + "v2/invoicing/invoices/" + invoiceId + "/send");
+        if (response.statusCode() >= 300 || response.statusCode() < 200) {
+            System.out.println("error on sending a bill bill");
+            System.out.println("response body - " + response.body().prettyPrint());
+        }
+        String billLink = response.body().jsonPath().getString("href");
+        return billLink;
     }
 
     public static String getNextInvoiceNumber() {
